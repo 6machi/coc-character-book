ed523b0474ed90d138061cb26df6d86ff634117d
6machi <6machi.trpg@gmail.com>
2026-05-05 13:22:45 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// Supabase接続設定
+window.COCBOOK_SUPABASE_URL = "https://rcjicwyeckyuhgknimyu.supabase.co/rest/v1/";
+window.COCBOOK_SUPABASE_ANON_KEY = "sb_publishable_3ROrVWxH1PV7PItkMgK4QA_TuTHWA2T";
